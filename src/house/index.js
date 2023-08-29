2fb4002a5460ddd52d34750f7b7f6fb0ee007e58
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-08-29 18:05:08 +0100

    Added house inquiry functionality

--- src/house/index.js
+++ src/house/index.js
@@ -1,6 +1,13 @@
 import "./house.css";
+import { useState } from "react";
+import emailIcon from "./Email.png";
+import Inquiry from "./Inquiry";
 
 const House = ({ house }) => {
+    const [inquiryShown, setInquiryShown] = useState(false);
+    const inquiryClick = () => {
+        setInquiryShown(!inquiryShown);
+    };
     return (
         <div>
             <div className="row mt-2">
@@ -16,10 +23,17 @@ const House = ({ house }) => {
                 <div className="col-md-5">
                     <p className="price">${house.price}</p>
                     <p>{house.description}</p>
+                    <img
+                        src={emailIcon}
+                        height="50"
+                        alt="inquiry"
+                        onClick={inquiryClick}
+                    />
+                    {inquiryShown && <Inquiry house={house} />}
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default House;
