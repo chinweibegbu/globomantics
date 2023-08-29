ec451a7f7270b71b696c37f66610ad926637a239
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-08-29 14:29:42 +0100

    Added featured house component to landing page

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,12 +1,43 @@
 // import logo from './logo.svg';
+import { useEffect, useState, useMemo } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './main-page.css';
-import Header from "./header"
+import Header from "./header";
+import FeaturedHouse from './featured-house';
 
 function App() {
+  // Watch the state of the houses
+  // NOTE: Passing [] to useState() makes allHouses initially an empty array
+  const [allHouses, setAllHouses] = useState([]);
+
+  // Loading data only when called the first time
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, [])
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses])
+
   return (
-    <div className='container'>
-      <Header subtitle="Providing houses all over the world" title="" />
-    </div>
+    <Router>
+      <div className='container'>
+        <Header subtitle="Providing houses all over the world" title="" />
+        <Switch>
+          <Route path='/'>
+            <FeaturedHouse house={featuredHouse} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
