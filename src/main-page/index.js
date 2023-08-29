2a03b12e4f8ff52d90dce1abcc7c34b34fe51a67
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-08-29 13:55:13 +0100

    Created first app component

---
+++ src/main-page/index.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './main-page.css';
+import Header from "./header"
+
+function App() {
+  return (
+    <div className='container'>
+      <Header subtitle="Providing houses all over the world" title="" />
+    </div>
+  );
+}
+
+export default App;
